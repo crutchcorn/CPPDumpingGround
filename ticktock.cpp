77170353cc246f6a6d966143c0052b0a095827c2
Corbin Crutchley <crutchcorn@gmail.com>
2016-08-03 22:23:53 -0700

    First step of adding commenting

--- ticktock.cpp
+++ ticktock.cpp
@@ -1,17 +1,29 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
+// Includes - They define the commands we'll be using
+#include <iostream> // General output & more
+#include <fstream> // File manipulation (Input & output)
+#include <string> // Strings. AKA Words EG: "Hello"
+#include <vector> // Vectors. Like arrays (list of items), but you can add things to this list. 
+// You cannot do this with arrays (you set how big an array is, and it will never change)
 
-typedef struct
+typedef struct 
+// A struc(ture) is a collection of data under one name
+// See below for typedef explaination
 {
-	std::string question;
-	std::vector<std::string> answers;
-	int correct;
-	bool asked;
-} question_t;
+	std::string question; // Makes a string variable called question
+	// std:: allows your computer to know where to find this command. This case, string is in the STANDARD library.
+	std::vector<std::string> answers; // Makes a vector (list) of strings in a variable called answers.
+	int correct; // Makes a variable called correct that is an int (AKA whole number)
+	bool asked; // Makes a boolean variable called asked. Only two values allowed; True or false
+} question_t; // typedef allows you to make a datatype (much like string or int) with a name of your choosing.
+// In this case, we are making a type called question_t is a mirror of sorts for the structure we just made.
 
 std::vector<question_t> loadQuestions()
+/*
+When you think, you can think in any language, but when you talk you have to speak the language that the person you're talking to speaks
+Just like this, you can "think" however you want but it has to make sense to the computer. We tell the computer how we're going to answer (with what data)
+by specifying what datatype we will return (speak) after we think. In this case, we are returning a vector of question_ts.
+The function (thought) is called loadQuestions, and can called by the computer to calculate and return data.
+*/ 
 {
 	std::vector<question_t> out = {};
 	std::vector<std::string> filestr = {"q_default_1", "q_default_2"};
